39ebed310820fce6a853c9187b8ee6d1719c10d8
Alan Penrod <alanpenrod6@gmail.com>
2024-12-04 19:43:40 -0500

    Attempt at side elements of marker details

--- capstone-project/vue/src/components/MapDisplay.vue
+++ capstone-project/vue/src/components/MapDisplay.vue
@@ -1,13 +1,20 @@
 <template>
-  <div>
-    <select v-on:change="filterMarkers($event.target.value)">
-      <option v-bind:value="'all'">All</option>
-      <option v-bind:value="1">Museums</option>
-      <option v-bind:value="2">Stadiums</option>
-      <option v-bind:value="3">Parks</option>
-      <option v-bind:value="4">Restaurants</option>
-      <option v-bind:value="5">Bars</option>
-    </select>
+  <div class="map-container">
+    <div class="sidebar">
+      <select v-on:change="filterMarkers($event.target.value)">
+        <option v-bind:value="'all'">All</option>
+        <option v-bind:value="1">Museums</option>
+        <option v-bind:value="2">Stadiums</option>
+        <option v-bind:value="3">Parks</option>
+        <option v-bind:value="4">Restaurants</option>
+        <option v-bind:value="5">Bars</option>
+      </select>
+      <ul>
+        <li v-for="marker in filteredMarkers" v-bind:key="marker.title">
+          <!--Direct to directions page-----<router-link v-bind:to="">{{ marker.title }}</router-link>-->
+        </li>
+      </ul>
+    </div>
     <div id="map" ref="map"></div>
   </div>
 </template>
@@ -21,37 +28,52 @@ export default {
       map: null,
       markers: [],
       markersData: [
-        {lat: 39.9656, lng: -75.1800, title: "Phaladelphia Museum of Art", groupId: 1},
-        {lat: 39.9583, lng: -75.1726, title: "The Franklin Institute", groupId: 1},
-        {lat: 39.9533, lng: -75.1766, title: "Mutter Museum", groupId: 1},
-        {lat: 39.9528, lng: -75.1481, title: "Nattional Constitution Center", groupId: 1},
-        {lat: 39.9489, lng: -75.1467, title: "Museum of the American Revolution", groupId: 1},
+        { lat: 39.9656, lng: -75.1800, title: "Phaladelphia Museum of Art", groupId: 1 },
+        { lat: 39.9583, lng: -75.1726, title: "The Franklin Institute", groupId: 1 },
+        { lat: 39.9533, lng: -75.1766, title: "Mutter Museum", groupId: 1 },
+        { lat: 39.9528, lng: -75.1481, title: "Nattional Constitution Center", groupId: 1 },
+        { lat: 39.9489, lng: -75.1467, title: "Museum of the American Revolution", groupId: 1 },
 
-        {lat: 39.9062, lng: -75.1675, title: "Citizens Bank Park", groupId: 2},
-        {lat: 39.9008, lng: -75.1675, title: "Lincoln Financial Field", groupId: 2},
-        {lat: 39.9012, lng: -75.1720, title: "Wells Fargo Center", groupId: 2},
-        {lat: 39.9523, lng: -75.1905, title: "Franklin Field", groupId: 2},
-        {lat: 40.0171, lng: -75.1527, title: "Marcus Foster Memorial Stadium", groupId: 2},
+        { lat: 39.9062, lng: -75.1675, title: "Citizens Bank Park", groupId: 2 },
+        { lat: 39.9008, lng: -75.1675, title: "Lincoln Financial Field", groupId: 2 },
+        { lat: 39.9012, lng: -75.1720, title: "Wells Fargo Center", groupId: 2 },
+        { lat: 39.9523, lng: -75.1905, title: "Franklin Field", groupId: 2 },
+        { lat: 40.0171, lng: -75.1527, title: "Marcus Foster Memorial Stadium", groupId: 2 },
 
-        {lat: 39.9486, lng: -75.1715, title: "Rittenhouse Square", groupId: 3},
-        {lat: 39.9042, lng: -75.1809, title: "FDR Park", groupId: 3},
-        {lat: 39.9449, lng: -75.1413, title: "Spruce Street Harbor Park", groupId: 3},
-        {lat: 39.9510, lng: -75.1684, title: "John F. Collins Park", groupId: 3},
-        {lat: 39.9479, lng: -75.1513, title: "Washington Square Park", groupId: 3},
+        { lat: 39.9486, lng: -75.1715, title: "Rittenhouse Square", groupId: 3 },
+        { lat: 39.9042, lng: -75.1809, title: "FDR Park", groupId: 3 },
+        { lat: 39.9449, lng: -75.1413, title: "Spruce Street Harbor Park", groupId: 3 },
+        { lat: 39.9510, lng: -75.1684, title: "John F. Collins Park", groupId: 3 },
+        { lat: 39.9479, lng: -75.1513, title: "Washington Square Park", groupId: 3 },
 
-        {lat: 39.9721, lng: -75.1286, title: "Bastia", groupId: 4},
-        {lat: 39.9456, lng: -75.1677, title: "Rex at the Royal", groupId: 4},
-        {lat: 39.9425, lng: -75.1450, title: "Provenance", groupId: 4},
-        {lat: 39.9478, lng: -75.1624, title: "Little Nonna's", groupId: 4},
+        { lat: 39.9721, lng: -75.1286, title: "Bastia", groupId: 4 },
+        { lat: 39.9456, lng: -75.1677, title: "Rex at the Royal", groupId: 4 },
+        { lat: 39.9425, lng: -75.1450, title: "Provenance", groupId: 4 },
+        { lat: 39.9478, lng: -75.1624, title: "Little Nonna's", groupId: 4 },
 
-        {lat: 39.9506, lng: -75.1620, title: "McGillin's Olde Ale House", groupId: 5},
-        {lat: 39.9475, lng: -75.1444, title: "Khyber Pass Pub", groupId: 5},
-        {lat: 39.9583, lng: -75.1703, title: "Assembly Rooftop Lounge", groupId: 5},
-        {lat: 39.9473, lng: -75.1439, title: "48 Record Bar", groupId: 5},
-        {lat: 39.9496, lng: -75.1503, title: "Independence Beer Garden", groupId: 5},
+        { lat: 39.9506, lng: -75.1620, title: "McGillin's Olde Ale House", groupId: 5 },
+        { lat: 39.9475, lng: -75.1444, title: "Khyber Pass Pub", groupId: 5 },
+        { lat: 39.9583, lng: -75.1703, title: "Assembly Rooftop Lounge", groupId: 5 },
+        { lat: 39.9473, lng: -75.1439, title: "48 Record Bar", groupId: 5 },
+        { lat: 39.9496, lng: -75.1503, title: "Independence Beer Garden", groupId: 5 },
       ],
+      selectedGroup: "all",
     };
   },
+  computed: {
+
+    filteredMarkers(){
+      let result;
+      if(this.selectedGroup === "all"){
+        result = this.markersData;
+      } else {
+        result = this.markersData.filter(marker => marker.groupId === this.selectedGroup);
+      }
+      return result;
+    },
+
+  },
+  
   mounted() {
     const loader = new Loader({
       apiKey: "AIzaSyB41DRUbKWJHPxaFjMAwdrzWzbVKartNGg", // Replace with your API key
@@ -68,7 +90,7 @@ export default {
 
       // Add event listener for pothole marker creation
       //.map.addListener("click", (event) => {
-        //this.createAttractionMarker(event.latLng);
+      //this.createAttractionMarker(event.latLng);
       //});
     });
   },
@@ -96,7 +118,7 @@ export default {
     },
 
     displayMarkers(groupId) {
-      
+
       this.markers.forEach(marker => marker.setMap(null));
       this.markers = [];
 
@@ -105,17 +127,18 @@ export default {
         filteredData = this.markersData;
       } else {
         filteredData = this.markersData.filter(marker => marker.groupId === groupId);
-      } 
+      }
 
 
       filteredData.forEach(markerData => {
         const marker = this.createMarker(markerData);
         this.markers.push(marker);
-      })
+      });
 
     },
 
     filterMarkers(groupId) {
+      this.selectedGroup = groupId;
       this.displayMarkers(groupId);
     }
   },
@@ -124,11 +147,21 @@ export default {
 
 <style scoped>
 
-#map {
-    height: 50vh;
-    width: 50vw;
+.map-container{
+  display: flex;
+  width: 100%;
+  height: 100vh;
 }
 
+.sidebar {
+  width: 25%;
+  padding: 10px;
+  background-color: #f8f9fa;
+  overflow-y: auto;
+}
 
-
+#map {
+  height: 50vh;
+  width: 50vw;
+}
 </style>
