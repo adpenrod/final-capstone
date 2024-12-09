22e28c7a409489202e365d0a78c524e32509637c
Alan Penrod <alanpenrod6@gmail.com>
2024-12-09 12:39:14 -0500

    table updates and style page push

--- capstone-project/vue/src/App.vue
+++ capstone-project/vue/src/App.vue
@@ -1,12 +1,13 @@
 <template>
   <div id="capstone-app">
-    <div id="nav">
-      <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
-      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
-      <router-link v-bind:to="{ name: 'login' }" v-else>Login</router-link>&nbsp;|&nbsp;
-      <router-link v-bind:to="{ name: 'maphome' }">Map</router-link>&nbsp;|&nbsp;
-      <router-link v-bind:to="{ name: 'badge' }">Rewards</router-link>&nbsp;|&nbsp;
-      <router-link v-bind:to="{ name: 'myhistory' }">My History</router-link>
+
+    <div class="tab">
+      <router-link class="tablinks" v-bind:class="{active: $route.name === 'home'}" v-bind:to="{ name: 'home' }">Home</router-link>
+      <router-link class="tablinks" v-bind:class="{active: $route.name === 'logout'}" v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
+      <router-link class="tablinks" v-bind:class="{active: $route.name === 'login'}" v-bind:to="{ name: 'login' }" v-else >Login</router-link>
+      <router-link class="tablinks" v-bind:class="{active: $route.name === 'maphome'}" v-bind:to="{ name: 'maphome' }">Map</router-link>
+      <router-link class="tablinks" v-bind:class="{active: $route.name === 'badge'}" v-bind:to="{ name: 'badge' }">Rewards</router-link>
+      <router-link class="tablinks" v-bind:class="{active: $route.name === 'myhistory'}" v-bind:to="{ name: 'myhistory' }">My History</router-link>
     </div>
     <router-view />
   </div>
@@ -25,6 +26,40 @@ export default {
 
 </script>
 
-<style>
+<style scoped>
+
+.tab{
+  overflow: hidden;
+  border: 1px solid #cccccc;
+  background-color: #f1f1f1;
+  display: flex;
+  justify-content: center;
+}
+
+.tab button{
+  background-color: #f1f1f1;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  padding: 14px 16px;
+  font-size: 16px;
+  color: #333333;
+}
+
+.tab button:hover{
+  background-color: #dddddd;
+}
+
+.tab .tablinks{
+  padding: 10px 20px;
+  font-size: 16px;
+  background-color: transparent;
+  cursor: pointer;
+}
+
+.tab .tablinks:hover{
+  background-color: azure;
+  color:#f1f1f1
+}
 
 </style>
