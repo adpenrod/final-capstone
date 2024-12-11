1f0f82d3a8d32da5168a3f6f9ab7bda8cf37b0d4
Anique Clements <abclem2@gmail.com>
2024-12-11 11:27:09 -0500

    homepage added

--- capstone-project/vue/src/views/HomeView.vue
+++ capstone-project/vue/src/views/HomeView.vue
@@ -1,26 +1,34 @@
 <template>
-  <div class="home">
-    <h1>Home</h1>
-    <p>You can either login/register or feel free to use our interactive map!</p>
-
-    <!--<input type="number" name="iLat" id="">-->
-
-
-    <img src="../assets/logos/home_page_banner_-_darker_720.png">
-
+  <section class= "hero">
+    <img src= "../assets/logos/homepage5.png" alt= "liberty banner" class="banner-image"/>
+    <div class="home">
+    <router-link to = "/login" class ="login-button"><button>Get Started</button></router-link>
   </div>
+  </section>
+   <!--make higher resolution image. Possibly remove text from banner. Add images or something pretty to login page.
+  -->
+    <!--<input type="number" name="iLat" id="">-->
 </template>
-
 <script>
-
 import MapDisplay from '../components/MapDisplay.vue';
-
 export default {
-
   components: {
     MapDisplay
   }
-  
 }
-
 </script>
+<style>
+.hero{
+  position: relative;
+  text-align: center;
+}
+.hero img {
+  width: 100%;
+  height: 100vh;
+}
+.home{
+  position: absolute;
+  top: 55%;
+  left: 77%;
+}
+</style>
